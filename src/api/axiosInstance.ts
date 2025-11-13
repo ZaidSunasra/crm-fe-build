c4bd51d3b2517cd850151e59f22118d3bce669f0
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-11-13 19:10:45 +0530

    use env variable for be url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const URL= import.meta.env.VITE_BE_URL;
+const URL= import.meta.env.VITE_BE_URL;
 
-const axiosInstance = axios.create({ baseURL: "https://api.myriadstoragesystem.com/api/v1", withCredentials: true });
+const axiosInstance = axios.create({ baseURL: URL, withCredentials: true });
 
 export default axiosInstance;
