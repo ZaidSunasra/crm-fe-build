9df63e846a70158c6f5a6b9480319be51c73352e
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-11-15 21:03:23 +0530

    Quotation body table UI changes

--- src/features/quotation/component/previewLayouts/PreviewBodyTable.tsx
+++ src/features/quotation/component/previewLayouts/PreviewBodyTable.tsx
@@ -3,24 +3,29 @@ import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, Table
 import React from "react";
 import type { AddQuotation } from "zs-crm-common";
 
-const PreviewBodyTable = ({data} : {data: AddQuotation}) => {
-    const { products, getProductItems} = useQuotation();
+const PreviewBodyTable = ({ data }: { data: AddQuotation }) => {
+    const { products, getProductItems } = useQuotation();
+    const compactorProducts = products.filter((product) =>
+        product.name.startsWith("Compactor")
+    );
     return (
         <>
-            {products.map((product) => {
-                const items = getProductItems(product.id)
+           {compactorProducts.map((product) => {
+                const items = getProductItems(product.id);
+                const isCompactor = product.name.startsWith("Compactor");
+                if(!isCompactor) return <></>
                 return (
                     <Table key={product.id}>
                         <TableHeader>
                             <TableRow>
-                                <TableHead rowSpan={2} className="border border-black">Sr. No</TableHead>
-                                <TableHead colSpan={3} className="border border-black max-w-sm">Description of Goods</TableHead>
-                                <TableHead rowSpan={2} className="border border-black">Installation Body</TableHead>
+                                <TableHead rowSpan={2} className="border border-black text-center">Sr. No</TableHead>
+                                <TableHead colSpan={3} className="border border-black max-w-sm text-center">Description of Goods</TableHead>
+                                <TableHead rowSpan={2} className="border border-black text-center">Installation Body</TableHead>
                             </TableRow>
                             <TableRow>
-                                <TableHead className="border border-black">Name</TableHead>
-                                <TableHead className="border border-black">Per bay qty</TableHead>
-                                <TableHead className="border border-black"> Qty</TableHead>
+                                <TableHead className="border border-black text-center">Name</TableHead>
+                                <TableHead className="border border-black text-center">Per bay qty</TableHead>
+                                <TableHead className="border border-black text-center"> Qty</TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
@@ -29,11 +34,11 @@ const PreviewBodyTable = ({data} : {data: AddQuotation}) => {
                                     {
                                         item.name !== "DOOR" &&
                                         <TableRow key={item.id}>
-                                            <TableCell className="border border-black">{index + 1} </TableCell>
+                                            <TableCell className="border border-black text-center">{index + 1} </TableCell>
                                             <TableCell className="border border-black max-wd-sm whitespace-normal break-words">{item.name}</TableCell>
-                                            <TableCell className="border border-black">{item.per_bay_qty}</TableCell>
-                                            <TableCell className="border border-black">{item.quantity}</TableCell>
-                                            <TableCell className="border border-black">{Number(item.per_bay_qty) * Number(item.quantity)}</TableCell>
+                                            <TableCell className="border border-black text-center">{item.per_bay_qty}</TableCell>
+                                            <TableCell className="border border-black text-center">{item.quantity}</TableCell>
+                                            <TableCell className="border border-black text-center">{Number(item.per_bay_qty) * Number(item.quantity)}</TableCell>
                                         </TableRow>
                                     }
                                 </React.Fragment>
@@ -43,15 +48,15 @@ const PreviewBodyTable = ({data} : {data: AddQuotation}) => {
                         <TableFooter>
                             <TableRow className="border-white">
                                 <TableCell colSpan={2} className="bg-background"></TableCell>
-                                <TableCell colSpan={2} className="border border-black">Total Body</TableCell>
-                                <TableCell className="border border-black">{product.total_body}</TableCell>
+                                <TableCell colSpan={2} className="border border-black text-center">Total Body</TableCell>
+                                <TableCell className="border border-black text-center">{product.total_body}</TableCell>
                             </TableRow>
                             {data.quotation_template === "set_wise" &&
                                 <TableRow>
                                     <TableCell colSpan={2} className="bg-background"></TableCell>
-                                    <TableCell className="border border-black border-r-0">Set</TableCell>
-                                    <TableCell className="border border-black border-l-0">{product.set}</TableCell>
-                                    <TableCell className="border border-black">{product.total_body * product.set}</TableCell>
+                                    <TableCell className="border border-black border-r-0 text-center">Set</TableCell>
+                                    <TableCell className="border border-black text-center">{product.set}</TableCell>
+                                    <TableCell className="border border-black text-center">{product.total_body * product.set}</TableCell>
                                 </TableRow>
                             }
                         </TableFooter>
--- src/features/quotation/component/printLayouts/QuotationBodyTable.tsx
+++ src/features/quotation/component/printLayouts/QuotationBodyTable.tsx
@@ -11,14 +11,14 @@ const QuotationBodyTable = ({ product, isSetWise }: { product: Quotation_Product
     return <Table className=" w-3/5" key={product.id}>
         <TableHeader>
             <TableRow>
-                <TableHead rowSpan={2} className="border border-black">Sr. No</TableHead>
-                <TableHead colSpan={3} className="border border-black">Description of Goods</TableHead>
-                <TableHead rowSpan={2} className="border border-black">Installation Body</TableHead>
+                <TableHead rowSpan={2} className="border border-black text-center">Sr. No</TableHead>
+                <TableHead colSpan={3} className="border border-black text-center">Description of Goods</TableHead>
+                <TableHead rowSpan={2} className="border border-black text-center">Installation Body</TableHead>
             </TableRow>
             <TableRow>
-                <TableHead className="border border-black">Per Bay Qty</TableHead>
-                <TableHead className="border border-black">Qty</TableHead>
-                <TableHead className="border border-black">Total</TableHead>
+                <TableHead className="border border-black text-center">Per Bay Qty</TableHead>
+                <TableHead className="border border-black text-center">Qty</TableHead>
+                <TableHead className="border border-black text-center">Total</TableHead>
             </TableRow>
         </TableHeader>
         <TableBody>
@@ -26,14 +26,14 @@ const QuotationBodyTable = ({ product, isSetWise }: { product: Quotation_Product
                 return <React.Fragment key={item.id}>
                     {item.item_name !== "DOOR" &&
                         <TableRow key={item.id}>
-                            <TableCell className="border border-black">{index + 1}</TableCell>
+                            <TableCell className="border border-black text-center">{index + 1}</TableCell>
                             <TableCell className="border border-black">
-                                <div>  {item.item_code} {product.name[0]} Bay </div>
+                                <div>  {item.item_code} {product.name.split(" ")[4]} Bay </div>
                                 <div> Body in {item.item_code}</div>
                             </TableCell>
-                            <TableCell className="border border-black">{item.per_bay_qty}</TableCell>
-                            <TableCell className="border border-black">{item.quantity}</TableCell>
-                            <TableCell className="border border-black">{Number(item.per_bay_qty) * Number(item.quantity)}</TableCell>
+                            <TableCell className="border border-black text-center">{item.per_bay_qty}</TableCell>
+                            <TableCell className="border border-black text-center">{item.quantity}</TableCell>
+                            <TableCell className="border border-black text-center">{Number(item.per_bay_qty) * Number(item.quantity)}</TableCell>
                         </TableRow>
                     }
                 </React.Fragment>
@@ -42,15 +42,15 @@ const QuotationBodyTable = ({ product, isSetWise }: { product: Quotation_Product
         <TableFooter>
             <TableRow className="border-white">
                 <TableCell colSpan={2} className="bg-background"></TableCell>
-                <TableCell colSpan={2} className="border border-black">Total Body</TableCell>
-                <TableCell className="border border-black">{product.quotation_working[0].total_body}</TableCell>
+                <TableCell colSpan={2} className="border border-black text-center">Total Body</TableCell>
+                <TableCell className="border border-black text-center">{product.quotation_working[0].total_body}</TableCell>
             </TableRow>
             {isSetWise &&
                 <TableRow>
                     <TableCell colSpan={2} className="bg-background"></TableCell>
-                    <TableCell className="border border-black border-r-0">Set</TableCell>
-                    <TableCell className="border border-black border-l-0">{product.quotation_working[0].set}</TableCell>
-                    <TableCell className="border border-black">{product.quotation_working[0].total_body * product.quotation_working[0].set}</TableCell>
+                    <TableCell className="border border-black text-center">Set</TableCell>
+                    <TableCell className="border border-black border-l-0 text-center">{product.quotation_working[0].set}</TableCell>
+                    <TableCell className="border border-black text-center">{product.quotation_working[0].total_body * product.quotation_working[0].set}</TableCell>
                 </TableRow>
             }
         </TableFooter>
